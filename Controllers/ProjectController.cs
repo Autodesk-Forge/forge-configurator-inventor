b7f0cd04f70aedbbf5e0193d4380fa4f7e950228
Scott Anderson <scott.anderson@autodesk.com>
2020-03-17 13:57:55 -0700

    Repurpose weather forecast controller from template for projects controller

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace IoConfigDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjectController : ControllerBase
+    {
+        private static readonly string[] Names = new[]
+        {
+            "Project1", "Project2", "Project3", "Project4", "Project5", "Project6", "Project7", "Project8", "Project9", "Project10"
+        };
+
+        private readonly ILogger<ProjectController> _logger;
+
+        public ProjectController(ILogger<ProjectController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Project> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Project
+            {
+                Date = DateTime.Now.AddDays(index),
+                Name = Names[rng.Next(Names.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Project.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IoConfigDemo
+{
+    public class Project
+    {
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
