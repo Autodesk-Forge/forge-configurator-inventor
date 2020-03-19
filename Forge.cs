120a21f93a388bf9a3f91b42f6750329d14bba0e
Scott Anderson <scott.anderson@autodesk.com>
2020-03-18 20:07:21 -0700

    Fix whitespace

--- Forge.cs
+++ Forge.cs
@@ -7,14 +7,14 @@ using Autodesk.Forge.Client;
 using Autodesk.Forge.Model;
 
 namespace IoConfigDemo {
-	class Forge {
-		// Initialize the oAuth 2.0 client configuration fron enviroment variables
-		private static string FORGE_CLIENT_ID = Environment.GetEnvironmentVariable ("FORGE_CLIENT_ID");
-		private static string FORGE_CLIENT_SECRET = Environment.GetEnvironmentVariable ("FORGE_CLIENT_SECRET");
-		private static Scope[] _scope = new Scope[] { Scope.DataRead };
+    class Forge {
+        // Initialize the oAuth 2.0 client configuration fron enviroment variables
+        private static string FORGE_CLIENT_ID = Environment.GetEnvironmentVariable ("FORGE_CLIENT_ID");
+        private static string FORGE_CLIENT_SECRET = Environment.GetEnvironmentVariable ("FORGE_CLIENT_SECRET");
+        private static Scope[] _scope = new Scope[] { Scope.DataRead };
 
-		// Intialize the 2-legged oAuth 2.0 client.
-		private static TwoLeggedApi _twoLeggedApi =new TwoLeggedApi() ;
+        // Intialize the 2-legged oAuth 2.0 client.
+        private static TwoLeggedApi _twoLeggedApi =new TwoLeggedApi() ;
 
         private string _twoLeggedAccessToken;
         private async Task<string> GetTwoLeggedAccessToken()
@@ -33,25 +33,25 @@ namespace IoConfigDemo {
             return _twoLeggedAccessToken;
         }
 
-		private static async Task<dynamic> _2leggedAsync () {
-			try {
-				// Call the asynchronous version of the 2-legged client with HTTP information
-				// HTTP information will help you to verify if the call was successful as well
-				// as read the HTTP transaction headers.
-				ApiResponse<dynamic> bearer = await _twoLeggedApi.AuthenticateAsyncWithHttpInfo(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, oAuthConstants.CLIENT_CREDENTIALS, _scope);
-				
+        private static async Task<dynamic> _2leggedAsync () {
+            try {
+                // Call the asynchronous version of the 2-legged client with HTTP information
+                // HTTP information will help you to verify if the call was successful as well
+                // as read the HTTP transaction headers.
+                ApiResponse<dynamic> bearer = await _twoLeggedApi.AuthenticateAsyncWithHttpInfo(FORGE_CLIENT_ID, FORGE_CLIENT_SECRET, oAuthConstants.CLIENT_CREDENTIALS, _scope);
+                
                 if (bearer.StatusCode != 200)
                 {
-					throw new Exception ("Request failed! (with HTTP response " + bearer.StatusCode + ")") ;
+                    throw new Exception ("Request failed! (with HTTP response " + bearer.StatusCode + ")") ;
                 }
 
-				// The JSON response from the oAuth server is the Data variable and has been
-				// already parsed into a DynamicDictionary object.
-				return bearer.Data;
-			} catch {
-				return null;
-			}
-		}
+                // The JSON response from the oAuth server is the Data variable and has been
+                // already parsed into a DynamicDictionary object.
+                return bearer.Data;
+            } catch {
+                return null;
+            }
+        }
 
         public async Task<List<ObjectDetails>> GetBucketObjects(string bucketKey)
         {
@@ -82,5 +82,5 @@ namespace IoConfigDemo {
 
             return objects;
         }
-	}
+    }
 }
