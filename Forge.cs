f11d8a2c042258957fd5b5ac4fffde8696b2ab95
Eugene Rymski <eugene.rymski@autodesk.com>
2020-03-24 11:04:12 +0300

    "Ensure bucket" implementation

--- Forge.cs
+++ Forge.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 using Autodesk.Forge;
+using Autodesk.Forge.Client;
 using Autodesk.Forge.Core;
 using Autodesk.Forge.Model;
+using Microsoft.AspNetCore.Http;
 
 namespace IoConfigDemo
 {
     class Forge : IForge
     {
-        private static readonly Scope[] _scope = { Scope.DataRead };
+        private static readonly Scope[] _scope = { Scope.DataRead, Autodesk.Forge.Scope.BucketCreate, Autodesk.Forge.Scope.BucketRead };
 
         // Initialize the 2-legged oAuth 2.0 client.
         private static readonly TwoLeggedApi _twoLeggedApi = new TwoLeggedApi();
@@ -44,7 +46,7 @@ namespace IoConfigDemo
             // HTTP information helps to verify if the call was successful as well as read the HTTP transaction headers.
             Autodesk.Forge.Client.ApiResponse<dynamic> response = await _twoLeggedApi.AuthenticateAsyncWithHttpInfo(_options.ClientId, _options.ClientSecret, oAuthConstants.CLIENT_CREDENTIALS, _scope);
 
-            if (response.StatusCode != 200)
+            if (response.StatusCode != StatusCodes.Status200OK)
             {
                 throw new Exception("Request failed! (with HTTP response " + response.StatusCode + ")");
             }
@@ -55,14 +57,16 @@ namespace IoConfigDemo
 
         public async Task<List<ObjectDetails>> GetBucketObjects(string bucketKey)
         {
+            await EnsureBucket(bucketKey);
+
             ObjectsApi objectsApi = new ObjectsApi();
             objectsApi.Configuration.AccessToken = await GetTwoLeggedAccessToken();
-            List<ObjectDetails> objects = new List<ObjectDetails>();
+            var objects = new List<ObjectDetails>();
 
             dynamic objectsList = await objectsApi.GetObjectsAsync(bucketKey);
             foreach (KeyValuePair<string, dynamic> objInfo in new DynamicDictionaryItems(objectsList.items))
             {
-                ObjectDetails details = new ObjectDetails
+                var details = new ObjectDetails
                 {
                     BucketKey = objInfo.Value.bucketKey,
                     ObjectId = objInfo.Value.objectId,
@@ -75,6 +79,27 @@ namespace IoConfigDemo
             }
 
             return objects;
+        }
+
+        /// <summary>
+        /// Make sure the bucket is exists.  Create if necessary.
+        /// </summary>
+        /// <param name="bucketName">The bucket name.</param>
+        private async Task EnsureBucket(string bucketName)
+        {
+            var api = new BucketsApi { Configuration = { AccessToken = await GetTwoLeggedAccessToken() }};
+
+            try
+            {
+                // the API call will throw an exception if the bucket is missing,
+                // so we don't care about the response, just need to ensure about succeeded call
+                await api.GetBucketDetailsAsync(bucketName);
+            }
+            catch (ApiException e) when(e.ErrorCode == StatusCodes.Status404NotFound) // try to create the bucket if error is "Not Found"
+            {
+                var payload = new PostBucketsPayload(bucketName, /*allow*/null, PostBucketsPayload.PolicyKeyEnum.Persistent);
+                await api.CreateBucketAsync(payload);
+            }
         }
     }
 }
