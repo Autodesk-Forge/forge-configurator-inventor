c5e57f34d645e08cc612f366ae9d2068c70b8536
Michal Hradil <michal.hradil@autodesk.com>
2020-04-01 15:46:27 +0200

    debug

--- create_creds.sh
+++ create_creds.sh
@@ -1,10 +1,2 @@
 #/bin/bash
-echo 'running create_creds.sh'
-cd output
-touch test.txt
-ls -lah
-echo 'test' > test.txt
-echo 'after test'
-ls -lah
-echo '{ "Forge": { "clientId": "$FORGE_CLIENT_ID", "clientSecret": "$FORGE_CLIENT_SECRET" } }' > appsettings.Local.json        
-echo 'creata_creds.sh finished'
+echo '{ "Forge": { "clientId": "$FORGE_CLIENT_ID", "clientSecret": "$FORGE_CLIENT_SECRET" } }'
