888d1345096df9a0a13c12400a62f746e3782707
Michal Hradil <michal.hradil@autodesk.com>
2020-04-01 16:18:59 +0200

    debug

--- create_creds.sh
+++ create_creds.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-echo '{ "Forge": { "clientId": "$FORGE_CLIENT_ID", "clientSecret": "$FORGE_CLIENT_SECRET" } }'
+echo '{ "Forge": { "clientId": "'$FORGE_CLIENT_ID'", "clientSecret": "'$FORGE_CLIENT_SECRET'" } }'
