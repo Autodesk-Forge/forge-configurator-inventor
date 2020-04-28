60bd9afc9a5ed6be5df667c8ec2b17f084dcf007
Zdenek Slavik <zdenek.slavik@autodesk.com>
2020-04-28 12:27:59 +0200

    INVGEN-39868 cleanup of console logs

--- WebApplication/ClientApp/src/components/forgeView.js
+++ WebApplication/ClientApp/src/components/forgeView.js
@@ -5,23 +5,6 @@ import { getActiveProject } from '../reducers/mainReducer';
 
 var Autodesk = null;
     
-/* Once the viewer has initialized, it will ask us for a forge token.
-We don't need to have a token to access our resouce, but need the viewer component to provide something with... */
-function handleTokenRequested(onAccessToken){
-    console.log('Token requested by the viewer.');
-    if(onAccessToken){
-        onAccessToken(null, null);
-    }
-}
-
-function onDocumentLoadFailure() {
-    console.error('Failed fetching Forge manifest');
-}    
-
-function handleViewerError(errorCode){
-    console.error('Error loading Forge Viewer. - errorCode:', errorCode);
-}  
-
 export class ForgeView extends Component {
 
     constructor(props){
@@ -38,47 +21,38 @@ export class ForgeView extends Component {
     display in our component */
     handleDocumentLoaded(doc, viewables){
         if (viewables.length === 0) {
-        console.error('Document contains no viewables.');
-        }
-        else{
-        //Select the first viewable in the list to use in our viewer component
-        this.setState({view:viewables[0]});
+            console.error('Document contains no viewables.');
+        } else {
+            //Select the first viewable in the list to use in our viewer component
+            this.setState({view:viewables[0]});
         }
     }
 
     handleScriptLoad(){
-        console.log('Autodesk scripts have finished loading.');
-    
-            let options = {
-                env: 'Local' // , getAccessToken: this.props.onTokenRequest
-            };
-    
-            Autodesk = window.Autodesk;
-            let container = this.viewerDiv.current;
-            this.viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('FV2'));
+        let options = {
+            env: 'Local' // , getAccessToken: this.props.onTokenRequest
+        };
 
-            Autodesk.Viewing.Initializer(
-                options, this.handleViewerInit.bind(this));
+        Autodesk = window.Autodesk;
+
+        let container = this.viewerDiv.current;
+        this.viewer = new Autodesk.Viewing.GuiViewer3D(container);
+
+        Autodesk.Viewing.Initializer(
+            options, this.handleViewerInit.bind(this));
       }
 
     handleViewerInit() {
-            console.log('Forge Viewer has finished initializing.');
+        var errorCode = this.viewer.start();
+        if (errorCode)
+            return;
+            
+        this.setState({enable:true});
 
-            console.log('Starting the Forge Viewer...');
-            var errorCode = this.viewer.start();
-            if (!errorCode){
-                    console.log('Forge Viewer has successfully started.');
-                    this.setState({enable:true});
-    
-                    Autodesk.Viewing.Document.load(
-                        this.props.activeProject.svf + '/bubble.json', this.onDocumentLoadSuccess.bind(this), onDocumentLoadFailure
-                      );
-                } else{
-              console.error('Error starting Forge Viewer - code:', errorCode);
-              handleViewerError(errorCode);
-            }
+        Autodesk.Viewing.Document.load(
+            this.props.activeProject.svf + '/bubble.json', this.onDocumentLoadSuccess.bind(this), () => {}
+        );
     }
-
 
     onDocumentLoadSuccess(viewerDocument) {
         var defaultModel = viewerDocument.getRoot().getDefaultGeometry();
@@ -88,11 +62,10 @@ export class ForgeView extends Component {
     render() {
         return (
             <div id="ForgeViewer">
-              <div id="FV2" ref={this.viewerDiv}></div>
+              <div ref={this.viewerDiv}></div>
               <link rel="stylesheet" type="text/css" href={`https://developer.api.autodesk.com/modelderivative/v2/viewers/7.*/style.css`}/>
               <Script url={`https://developer.api.autodesk.com/modelderivative/v2/viewers/7.*/viewer3D.js`}
                 onLoad={this.handleScriptLoad.bind(this)}
-                onError={handleViewerError}
               />
             </div>            
         )
