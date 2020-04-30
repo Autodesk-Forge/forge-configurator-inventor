ea734404725f1436211c4e17b3f3d92a4b3d4f5d
David Obergries <david.obergries@autodesk.com>
2020-04-30 10:56:22 +0200

    initialized real UI controls (editbox / checkbox / listbox)

--- WebApplication/ClientApp/src/components/parameter.js
+++ WebApplication/ClientApp/src/components/parameter.js
@@ -1,17 +1,101 @@
 import React, { Component } from 'react';
 import './parameters.css'
 
+import Input from '@hig/input';
+import Checkbox from '@hig/checkbox';
+import Dropdown from '@hig/dropdown';
+
 export class Parameter extends Component {
 
+    constructor(props) {
+        super(props);
+        this.onComboChange = this.onComboChange.bind(this);
+        this.onCheckboxChange = this.onCheckboxChange.bind(this);
+        this.onEditChange = this.onEditChange.bind(this);
+
+        this.state = {
+            value: null
+        }
+    }
+
+    onComboChange(data) {
+        console.log('onComboChange - ' + data);
+        this.setState({ value: data });
+    }
+
+    onCheckboxChange(data) {
+        this.setState({ value: data ? "True" : "False" });
+    }
+
+    onEditChange(data) {
+        this.setState( {value: data.target.value } );
+    }
+
+    onFocus(data) {
+        // ??
+    }
+
+    reset() {
+        // state will reset back to original values when hit UPDATE
+        this.setState({ value: this.prop.parameter.value });
+    }
+
     render() {
-        return (
+        if (this.state.value == null)
+            this.state.value = this.props.parameter.value;
+
+        if (this.props.parameter.units === "Boolean")
+            return (
+                <div className="parameter checkbox">
+                    <Checkbox
+                        disabled={false}
+                        indeterminate={false}
+                        onBlur={null}
+                        onChange={this.onCheckboxChange}
+                        onFocus={this.onFocus}
+                        onMouseDown={null}
+                        checked={this.state.value === "True"}
+                    />
+                    <div className="parameter checkboxtext">
+                        {this.props.parameter.name}
+                    </div>
+                </div>
+            )
+        else if (this.props.parameter.allowedValues != null && this.props.parameter.allowedValues.length > 0)
+            return (
+                <div className="parameter">
+                    {this.props.parameter.name}
+                    <Dropdown
+                        variant="box"
+                        disabled={false}
+                        error={false}
+                        required=""
+                        multiple={false}
+                        onBlur={null}
+                        onChange={this.onComboChange}
+                        onFocus={this.onFocus}
+                        options={this.props.parameter.allowedValues}
+                        value={this.state.value}
+                    />
+                </div>
+            )
+        else
+            return (
             <div className="parameter">
-                {this.props.parameter.name},
-                {this.props.parameter.value},
-                {this.props.parameter.type},
-                {this.props.parameter.units}
+                {this.props.parameter.name}
+                <Input
+                    disabled={false}
+                    onBlur={null}
+                    onChange={this.onEditChange}
+                    onFocus={this.onFocus}
+                    onMouseEnter={null}
+                    onMouseLeave={null}
+                    variant="box"
+                    value={this.state.value}
+                />
+
             </div>
-        )
+            )
     }
 }
 
