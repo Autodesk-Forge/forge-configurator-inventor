2f4bc257f0f48c4e2b5006f54e821c3a20b566ec
Eugene Rymski <erymski@gmail.com>
2020-04-30 17:12:54 +0300

    Ensure integration tests work

--- WebApplication/ClientApp/src/__tests__/Integration.test.js
+++ WebApplication/ClientApp/src/__tests__/Integration.test.js
@@ -5,7 +5,7 @@ const pageUrl = "https://localhost:5001";
 
 const config = {
     //headless: false, // NOTE: uncomment to see what is actually going o the screen
-    devtools: true,
+    // devtools: true, // NOTE: uncomment for debugging
     args: [
         // to avoid errors about self-signed SSL certificate: https://stackoverflow.com/q/55207690
         '--ignore-certificate-errors',
@@ -20,10 +20,18 @@ describe('Integration UI tests', () => {
     let browser = null;
     let page = null;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
 
         // TODO: try different browser kinds
         browser = await playwright["chromium"].launch(config);
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    beforeEach(async () => {
+
         page = await browser.newPage();
 
         if (!page) {
@@ -33,7 +41,7 @@ describe('Integration UI tests', () => {
         // report about requested URLs
         await page.route('**', async (request) => {
 
-            console.log(request.url());
+            // console.log(request.url());
             await request.continue();
         });
 
@@ -42,9 +50,13 @@ describe('Integration UI tests', () => {
     });
 
     afterEach(async () => {
-        await browser.close();
+        await page.close();
     });
 
+
+    async function waitForLoadedProjects() {
+        await page.waitFor('//p', { timeout: 3000 });
+    }
 
     it('should check the page is loaded', async () => {
 
@@ -53,24 +65,17 @@ describe('Integration UI tests', () => {
         expect(await page.title()).not.toBeNull();
     });
 
-    it(`should project loading`, async () => {
+    it(`should check projects list is loaded`, async () => {
 
         // wait until project list is refreshed.
         // This selector does not exist on page opening, and will be created after projects are loaded from the server. Wait for it.
         const listItems = await page.$$("#project-list ul li");
-        expect(listItems.length).toBeGreaterThan(0); // expects that
+        expect(listItems).toHaveLength(2); // expecting two default projects
     });
 
-    it('PROJECTS is loaded', async () => {
+    it('should check if Autodesk Forge link works', async () => {
 
-        // check initial project list is filled
-        const initialContent = await page.evaluate(() => document.evaluate('//p', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent);
-        expect(initialContent).toBe("Project1.zip");
-    });
-
-    it('Check Autodesk Forge link', async () => {
-
-        // check Forge link            
+        // check Forge link
         const link = await page.waitFor('a[aria-label="Autodesk HIG"]', {timeout: 3000});
         await link.click();
 
@@ -81,9 +86,9 @@ describe('Integration UI tests', () => {
         expect(await page.title()).toBe("Autodesk Forge");
     });
 
-    it('PROJECTS menu is loaded', async () => {
+    it('should check Projects switcher is loaded', async () => {
 
-        // wait until project combo is displayed        
+        // wait until project combo is displayed
         const project = await page.waitFor('//p',{timeout: 3000});
         await project.click();
 
@@ -96,46 +101,46 @@ describe('Integration UI tests', () => {
 
         // check name of the first project
         const firstDemoProject = await page.evaluate(() => document.evaluate('//ul/li[1]/span[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent);
-        expect(firstDemoProject).toBe("Project1.zip");
+        expect(firstDemoProject).toBe("Conveyor.zip");
 
         // check name of the second project
         const secondDemoProject = await page.evaluate(() => document.evaluate('//ul/li[2]/span[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent);
-        expect(secondDemoProject).toBe("Project2.zip");
+        expect(secondDemoProject).toBe("Wrench.zip");
     });
 
-    it('check if the right PROJECT is set', async () => {
+    it('should check if Project switcher allows to switch project', async () => {
 
         // wait until project combo is displayed
         const project = await page.waitFor('//p',{timeout: 3000});
         await project.click();
-        
+
         // wait until project list is displayed
         await page.waitForSelector('//div/div[2]', {visible: true, timeout: 3000});
 
         // emulate click to trigger project loading
         await page.evaluate(() => document.evaluate('//ul/li[1]/span[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click());
         let project_name = await page.evaluate(() => document.evaluate('//p', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent);
-        expect(project_name).toBe("Project1.zip");
+        expect(project_name).toBe("Conveyor.zip");
 
         // click to show popup menu with list of projects
         await page.evaluate(() => document.evaluate('//p', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click());
-        
+
         // wait until project list is displayed
         await page.waitForSelector('//div/div[2]', {visible: true, timeout: 3000});
 
         // emulate click to trigger project loading
         await page.evaluate(() => document.evaluate('//ul/li[2]/span[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click());
         project_name = await page.evaluate(() => document.evaluate('//p', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent);
-        expect(project_name).toBe("Project2.zip");        
+        expect(project_name).toBe("Wrench.zip");
      });
 
-     it('Check Log Button', async () => {
+     it('should check presence of Log button', async () => {
 
         // check if exists the button
         const button = await page.waitFor('//div/div[1]/button');
         expect(button).not.toBeNull();
         await button.click();
-        
+
         // wait until log popup is displayed
         await page.waitForSelector('//div/div[2]', {visible: true, timeout: 3000});
 
@@ -143,26 +148,18 @@ describe('Integration UI tests', () => {
         expect(log_description).toBe("Navigation Action");
     });
 
-    it('Check User Button', async () => {
+    it('should check presence of User button', async () => {
 
         // check if exists the button
         await page.waitFor('span[aria-label="Avatar for anonymous user"]', {timeout: 2000});
 
         // validate user name
         const text = await page.evaluate(() => document.evaluate('//button/span/span', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.textContent);
-        expect(text).toBe("AU") 
+        expect(text).toBe("AU")
     });
 
-    /*
-    it('test version', async () =>{
-        await page.goto("https://localhost:5001/version", {waitUntil: 'networkidle0'});
-        const version = await page.evaluate(() => document.body.textContent);
-
-        expect(version).toBe("1.0.1");
-    });
-    */
-    it('checks Version endpoint', async () => {
+    it('should check Version endpoint', async () => {
         await page.goto(pageUrl + '/Version');
         expect(await page.$('pre')).not.toBeNull();
-    })
+    });
 });
