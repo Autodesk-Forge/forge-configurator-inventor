6fe06fd66e3eeac286c5e487b26ecf5b0fb0db03
Eugene Rymski <erymski@gmail.com>
2020-05-08 13:52:56 +0300

    Listen store for notification changes and stream log messages into browser console

--- WebApplication/ClientApp/src/index.js
+++ WebApplication/ClientApp/src/index.js
@@ -12,6 +12,38 @@ import {mainReducer} from './reducers/mainReducer';
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(mainReducer);
 
+// based on https://github.com/reduxjs/redux/issues/303#issuecomment-125184409
+function observeStore(store, select, onChange) {
+    let currentState;
+
+    function handleChange() {
+        const nextState = select(store.getState());
+        if (nextState !== currentState) {
+            onChange(currentState, nextState);
+            currentState = nextState;
+        }
+    }
+
+    const unsubscribe = store.subscribe(handleChange);
+    handleChange();
+    return unsubscribe;
+}
+
+const logSelector = (state) => state.notifications;
+
+/** Dump log messages into browser console */
+function logToConsole(oldMessages, newMessages) { // TODO: not clear how to differentiate 'information' from 'error' messages
+
+  const from = oldMessages ? oldMessages.length : 0;
+    for (let i = from; i < newMessages.length; i++) {
+        // eslint-disable-next-line no-console
+        console.log(newMessages[i]);
+    }
+}
+
+// listen for notifications changes and stream them into console
+observeStore(store, logSelector, logToConsole);
+
 // const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
