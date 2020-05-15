abd5218b5c80fb0e01b0d4300779bfc4957ae866
Zdenek Slavik <zdenek.slavik@autodesk.com>
2020-05-15 10:55:28 +0200

    INVGEN-40845 Codecept test for viewer to load

---
+++ WebApplication/ClientApp/src/__tests__/viewer_test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+
+Before((I) => {
+    I.amOnPage('https://localhost:5001');
+});
+
+Feature('Viewer');
+
+Scenario('should check switch to model tab loads the viewer', async (I) => {
+
+    const modelTabSelector = '.tabsContainer li:nth-of-type(2) p';
+    const viewerSelector = '.canvas-wrap canvas';
+    const viewerModelSelector = '#ViewerModelStructurePanel';
+
+    I.see('Model', modelTabSelector);
+    I.wait(5); // allow the projects to load
+    I.click(modelTabSelector);
+    I.waitForElement(viewerSelector, 20);
+    I.waitForElement(viewerModelSelector, 20);
+});
