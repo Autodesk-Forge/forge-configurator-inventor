c6bc3592cd01e985a2b3ddd23f11528fbbc1eb6d
David Obergries <david.obergries@autodesk.com>
2020-05-15 09:09:14 +0200

    removed buttons DIV

--- WebApplication/ClientApp/src/components/parametersContainer.js
+++ WebApplication/ClientApp/src/components/parametersContainer.js
@@ -37,24 +37,22 @@ export class ParametersContainer extends Component {
                 </div>
                 <hr className="parametersSeparator"/>
                 <div className={buttonsContainerClass}>
-                    <div className="buttons">
-                        <Button style={{width: '125px'}}
-                            size="standard"
-                            title="Reset"
-                            type="secondary"
-                            width="grow"
-                            onClick={() => {this.props.resetParameters(this.props.activeProject.id, this.props.projectSourceParameters);}}
-                        />
-                        <div style={{width: '14px'}}/>
-                        <Button style={{width: '125px'}}
-                            size="standard"
-                            title="Update"
-                            type="primary"
-                            width="grow"
-                            onClick={() => {this.updateClicked();}}
-                        />
-                        <Modal open={this.props.updateProgressShowing} title="Wait please" id="Modal" name="Modal" onCloseClick={() => this.closeModal()}>Model is now being updated based on the changed parameters.</Modal>
-                    </div>
+                    <Button style={{width: '125px'}}
+                        size="standard"
+                        title="Reset"
+                        type="secondary"
+                        width="grow"
+                        onClick={() => {this.props.resetParameters(this.props.activeProject.id, this.props.projectSourceParameters);}}
+                    />
+                    <div style={{width: '14px'}}/>
+                    <Button style={{width: '125px'}}
+                        size="standard"
+                        title="Update"
+                        type="primary"
+                        width="grow"
+                        onClick={() => {this.updateClicked();}}
+                    />
+                    <Modal open={this.props.updateProgressShowing} title="Wait please" id="Modal" name="Modal" onCloseClick={() => this.closeModal()}>Model is now being updated based on the changed parameters.</Modal>
                 </div>
             </div>
         );
