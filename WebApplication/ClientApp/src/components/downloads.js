b47f9dc0c2dd9600468df0d3ccc9a3b6c7942eae
Zdenek Slavik <zdenek.slavik@autodesk.com>
2020-05-27 11:53:41 +0200

    icons and columns alignment fixed

--- WebApplication/ClientApp/src/components/downloads.js
+++ WebApplication/ClientApp/src/components/downloads.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import BaseTable, { AutoResizer } from 'react-base-table'
+import BaseTable, { AutoResizer, Column } from 'react-base-table'
 import 'react-base-table/styles.css'
 import { downloadFile } from '../actions/downloadActions'
+
+const Icon = ({ iconname }) => (
+    <div>
+      <img src={iconname} alt=''/>
+    </div>
+  )
 
 const columns = [
     {
         key: 'icon',
         title: '',
         dataKey: 'icon',
+        cellRenderer: ({ cellData: iconname }) => <Icon iconname={iconname} />,
+        align: Column.Alignment.RIGHT,
         width: 100,
     },    
     {
         key: 'type',
         title: 'File Type',
         dataKey: 'type',
+        align: Column.Alignment.LEFT,
         width: 150,
     },
     {
         key: 'env',
         title: 'Environment',
         dataKey: 'env',
+        align: Column.Alignment.CENTER,
         width: 200,
     }
 ];
@@ -27,13 +37,13 @@ const columns = [
   const data = [
     {
         id: 'updatedIam',
-        icon: null,
+        icon: 'products-and-services-24.svg',
         type: 'IAM',
         env: 'Model'
     },
     {
         id: 'rfa',
-        icon: null,
+        icon: 'products-and-services-24.svg',
         type: 'RFA',
         env: 'Model'
     }
