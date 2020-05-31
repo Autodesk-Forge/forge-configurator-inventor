2c7bcef2539c5650fbe78865e25168284e9ad8e9
slavikz-centrum <57212990+slavikz-centrum@users.noreply.github.com>
2020-05-31 20:59:04 +0200

    removed empty test, enhanced the ui test for downloads tab (#96)
    Co-authored-by: Zdenek Slavik <zdenek.slavik@autodesk.com>

--- WebApplication/ClientApp/src/ui-tests/downloads_test.js
+++ WebApplication/ClientApp/src/ui-tests/downloads_test.js
@@ -12,4 +12,9 @@ Scenario('should check switch to downloads tab shows the downloads links', async
     I.click(XPathElements.xpTabDownloads);
     I.waitForElement('.BaseTable');
     I.seeNumberOfElements('.BaseTable__row', 2);
+    // all expected download types are available
+    I.see('IAM', '.BaseTable__row-cell-text');
+    I.see('RFA', '.BaseTable__row-cell-text');
+    // check icons
+    I.seeNumberOfElements({ css: '[src="products-and-services-24.svg"]'}, 2);
 });
