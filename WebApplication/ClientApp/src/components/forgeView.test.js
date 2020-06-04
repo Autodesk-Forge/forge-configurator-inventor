06b1db33510ba6ad2efa40c33d63d56daabf1de6
Zdenek Slavik <zdenek.slavik@autodesk.com>
2020-06-04 12:56:22 +0200

    more test scenarios

--- WebApplication/ClientApp/src/components/forgeView.test.js
+++ WebApplication/ClientApp/src/components/forgeView.test.js
@@ -5,20 +5,17 @@ import { ForgeView } from './forgeView';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const activeProject = {
-  id: '1',
-  label: 'New Project',
-  image: 'new_image.png',
-  svf: 'aaa111'
-};
+const viewerDocumentMock = {
+  getRoot: () => ({
+    getDefaultGeometry: () => ''
+  })
+}
 
-const baseProps = {
-  activeProject
-};
+const loadDocumentNodeMock = jest.fn();
 
 class GuiViewer3DMock {
   addEventListener = jest.fn();
-  loadDocumentNode = jest.fn();
+  loadDocumentNode = loadDocumentNodeMock;
   start = jest.fn();
 }
 
@@ -29,14 +26,21 @@ const AutodeskMock = {
       handleViewerInit();
     },
     Document: {
-      load: jest.fn()
+      load: (_, onLoadSuccess) => {
+        onLoadSuccess(viewerDocumentMock);
+      }
     }
   }
 }
 
+beforeEach(() => {
+  loadDocumentNodeMock.mockClear();
+});
+
 describe('components', () => {
   describe('ForgeView', () => {
-    it('ForgeView DOM core structure is as expected', () => {
+    it('load gets called when svf provided', () => {      
+      const baseProps = { activeProject: { svf: 'aaa111' } };  
       const wrapper = shallow(<ForgeView { ...baseProps } />);
 
       const viewer = wrapper.find('.viewer');
@@ -46,7 +50,31 @@ describe('components', () => {
 
       window.Autodesk = AutodeskMock;
       script.simulate('load');
-      expect(AutodeskMock.Viewing.Document.load).toBeCalled();
+      expect(loadDocumentNodeMock).toBeCalled();
     });
+
+    it('load gets called when svf changes', () => {
+      const baseProps = { activeProject: { svf: 'aaa111' } };  
+      const wrapper = shallow(<ForgeView { ...baseProps } />);
+
+      window.Autodesk = AutodeskMock;
+      const script = wrapper.find('Script');
+      script.simulate('load');
+
+      const updateProps = { activeProject: { svf: 'newurl' } };  
+      wrapper.setProps(updateProps);
+      expect(loadDocumentNodeMock).toHaveBeenCalledTimes(2);
+    });
+
+    it('returns without loading when svf is null', () => {
+      const baseProps = { activeProject: { svf: null } };  
+      const wrapper = shallow(<ForgeView { ...baseProps } />);
+
+      window.Autodesk = AutodeskMock;
+      const script = wrapper.find('Script');
+      script.simulate('load');
+
+      expect(loadDocumentNodeMock).toHaveBeenCalledTimes(0);
+    });    
   });
 });
