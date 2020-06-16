1afb17d9742e0cacc4fb0298bb1a37fae9eb135f
Eugene Rymski <erymski@gmail.com>
2020-06-16 17:40:54 +0300

    Generate unique bucket name for different deployments

--- WebApplication/State/UserResolver.cs
+++ WebApplication/State/UserResolver.cs
@@ -1,4 +1,6 @@
 using System.Threading.Tasks;
+using Autodesk.Forge.Core;
+using Microsoft.Extensions.Options;
 using WebApplication.Services;
 using WebApplication.Utilities;
 
@@ -11,14 +13,16 @@ namespace WebApplication.State
     {
         private readonly ResourceProvider _resourceProvider;
         private readonly IForgeOSS _forgeOSS;
+        private readonly ForgeConfiguration _forgeConfig;
 
         public string Token { private get; set; }
         public bool IsAuthenticated => ! string.IsNullOrEmpty(Token);
 
-        public UserResolver(ResourceProvider resourceProvider, IForgeOSS forgeOSS)
+        public UserResolver(ResourceProvider resourceProvider, IForgeOSS forgeOSS, IOptions<ForgeConfiguration> forgeConfiguration)
         {
             _resourceProvider = resourceProvider;
             _forgeOSS = forgeOSS;
+            _forgeConfig = forgeConfiguration.Value;
         }
 
         public async Task<OssBucket> GetBucket()
@@ -28,9 +32,11 @@ namespace WebApplication.State
                 var profile = await _forgeOSS.GetProfileAsync(Token);
                 var userId = profile.userId;
 
-                var userHash = Crypto.GenerateHashString(userId);
-
-                var bucketKey = $"authd-{userId.Substring(0, 3)}-{userHash}".ToLowerInvariant();
+                // an OSS bucket must have a unique name, so it should be generated in a way,
+                // so it a Forge user gets registered into several deployments it will not cause
+                // name collisions. So use client ID (as a salt) to generate bucket name.
+                var userHash = Crypto.GenerateHashString(_forgeConfig.ClientId + userId);
+                var bucketKey = $"authd-{_forgeConfig.ClientId}-{userId.Substring(0, 3)}-{userHash}".ToLowerInvariant();
 
                 var bucket = new OssBucket(_forgeOSS, bucketKey);
                 await bucket.CreateAsync();
