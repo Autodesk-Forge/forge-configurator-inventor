f0e6b28efb1f77c174f160edf918fc1bc03a788a
Zdenek Slavik <zdenek.slavik@autodesk.com>
2020-06-16 16:31:07 +0200

    fix test

--- WebApplication/ClientApp/src/components/projectList.test.js
+++ WebApplication/ClientApp/src/components/projectList.test.js
@@ -3,7 +3,21 @@ import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { ProjectList, projectListColumns } from './projectList';
 
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
 Enzyme.configure({ adapter: new Adapter() });
+
+// mock store
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const mockState = {
+  uiFlagsReducer: {
+    showUploadPackage: false,
+    package: { file: '', root: '' }
+  }
+};
 
 const projectList = {
     activeProject: {
@@ -34,8 +48,8 @@ describe('ProjectList components', () => {
     const wrapper = shallow(<ProjectList { ...props } />);
     const as = wrapper.find('AutoResizer');
     const bt = as.renderProp('children')( {width: 100, height: 200} );
-    expect(bt.prop('width')).toEqual(84);
-    expect(bt.prop('height')).toEqual(184);
+    expect(bt.prop('width')).toEqual(100); // no longer reducing the size
+    expect(bt.prop('height')).toEqual(200); // no longer reducing the size
   });
 
   it('Base table has expected columns', () => {
@@ -58,7 +72,14 @@ describe('ProjectList components', () => {
   });
 
   it('Base table renders expected count of links and icons', () => {
-    const wrapper = mount(<ProjectList { ...props } />);
+    let store = mockStore(mockState);
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <ProjectList { ...props } />
+      </Provider>
+    );
+
     const as = wrapper.find('AutoResizer');
     const bt = as.renderProp('children')( {width: 100, height: 200} );
     const icons = bt.find('Icon');
