baff5751f1be278b62b8fb5df0b4b3d78e42a70a
Michal Hradil <mhradil@seznam.cz>
2020-06-16 15:48:21 +0200

    INVGEN-41932 Improve automation error handling on the pipeline (#135)
    This PR brings new pipeline results reporting, PR check and tests being part of the build phase. Feel free to review.

--- AWS-CICD/sdra-windows-build/2.push.ps1
+++
@@ -1,3 +0,0 @@
-$psw=$(aws ecr get-login-password --region us-west-2)
-docker login -u AWS -p $psw 624240287035.dkr.ecr.us-west-2.amazonaws.com/sdra-windows-build
-docker push 624240287035.dkr.ecr.us-west-2.amazonaws.com/sdra-windows-build:4.8
