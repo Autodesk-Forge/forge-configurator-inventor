e287f298f44a9a5f56e8bee972d66029c9152906
Pavel Holecek <pavel.holecek@autodesk.com>
2020-06-17 11:01:10 +0200

    Adding tests for Sign-In and Sign-Out out.

---
+++ WebApplication/ClientApp/src/ui-tests/authentication_test.js
@@ -0,0 +1,34 @@
+	/* eslint-disable no-undef */
+    const assert = require('assert');
+    const avatarElement = '//button[@type="button"]//span[contains(@role, "img")]';
+    const avatarName = 'Demo Tool';
+    const avatarAnonymous = 'Anonymous';
+
+    Before((I) => {
+        I.amOnPage('/');
+    });
+
+    Feature('Authentication');
+
+    Scenario('check Sign-in workflow', async (I) => {
+
+        I.signIn();
+
+        // check Avatar name
+        I.waitUntil(() => document.readyState == "complete", 10);
+        const attr = await I.grabAttributeFrom(avatarElement, 'aria-label');
+        assert.equal(attr.toString().includes(avatarName), true, 'Incorrect Sign-in process! Avatar "adsk.inv" is not Singed-in!');
+
+    });
+
+    Scenario('check Sign-out workflow', async (I) => {
+        I.signIn();
+        I.waitUntil(() => document.readyState == "complete", 10);
+        I.signOut();
+
+        // check Avatar name
+        I.waitUntil(() => document.readyState == "complete", 10);
+        const attr = await I.grabAttributeFrom(avatarElement, 'aria-label');
+        assert.equal(attr.toString().includes(avatarAnonymous), true, 'Incorrect Sign-out process!');
+
+    });
