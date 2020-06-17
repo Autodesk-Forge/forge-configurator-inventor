7278a1f960060e6d91a0cf7313be081e4aa524ea
Zdenek Slavik <zdenek.slavik@autodesk.com>
2020-06-17 13:46:43 +0200

    moving isLoggedIn to store

--- WebApplication/ClientApp/src/reducers/profileReducer.js
+++ WebApplication/ClientApp/src/reducers/profileReducer.js
@@ -1,6 +1,7 @@
 import actions from '../actions/profileActions';
 
 export const initialState = {
+    isLoggedIn: false,
     name: "Anonymous",
     avatarUrl: "logo-xs-white-BG.svg"
 };
@@ -8,7 +9,7 @@ export const initialState = {
 export default function(state = initialState, action) {
     switch(action.type) {
         case actions.PROFILE_LOADED: {
-            return {name: action.profile.name, avatarUrl: action.profile.avatarUrl};
+            return { isLoggedIn: action.isLoggedIn, name: action.profile.name, avatarUrl: action.profile.avatarUrl};
         }
         default:
             return state;
