953239d8e911063fbcef276d794b3e410754178a
David Obergries <david.obergries@autodesk.com>
2020-06-18 17:02:17 +0200

    added upload progress modal test

--- WebApplication/ClientApp/src/components/modalProgress.test.js
+++ WebApplication/ClientApp/src/components/modalProgress.test.js
@@ -3,18 +3,19 @@ import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { ModalProgressUpdate } from './modalProgressUpdate';
 import { ModalProgressRfa } from './modalProgressRfa';
+import { ModalProgressUpload } from './modalProgressUpload';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-const props = {
-    title: "modal progress dialog title",
-    label: "name of file in progress",
-    icon: "Archive.svg"
-};
 
 describe('modal progress ', () => {
 
     it('should show message from props.label', () => {
+
+        const props = {
+            title: "modal progress dialog title",
+            label: "name of file in progress",
+            icon: "Archive.svg"
+        };
 
         const wrapper = shallow(<ModalProgressUpdate {...props} />);
 
@@ -58,4 +59,23 @@ describe('modal progress ', () => {
         expect(button.length).toBe(0);
     });
 
+    it('check that here is NO button available when is not update progress set to done', () => {
+
+        const props = { isDone: () => { return false;} };
+
+        const wrapper = shallow(<ModalProgressUpload {...props} />);
+
+        const button = wrapper.find('Button');
+        expect(button.length).toBe(0);
+    });
+
+    it('check that here are TWO buttons available when is update progress set to done', () => {
+
+        const props = { isDone: () => { return true;} };
+
+        const wrapper = shallow(<ModalProgressUpload {...props} />);
+
+        const buttons = wrapper.find('Button');
+        expect(buttons.length).toBe(2);
+    });
 });
