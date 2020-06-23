aff11dfca21d36af6790c05b2949d733b5c8886a
Michal Vasicek <michal.vasicek@autodesk.com>
2020-06-23 18:22:30 +0200

    Use https scheme only for production.

--- WebApplication/Controllers/LoginController.cs
+++ WebApplication/Controllers/LoginController.cs
@@ -40,7 +40,12 @@ namespace WebApplication.Controllers
             // prepare redirect URL for Oxygen
             // NOTE: This MUST match the pattern of the callback URL field of the app's registration
             // TODO: workaround which may be removed once application will start to use https
-            var callbackUrl = $"https{Uri.SchemeDelimiter}{HttpContext.Request.Host}";
+            var scheme = HttpContext.Request.Scheme;
+            if (HttpContext.Request.Host.Host == "inventor-config-demo.autodesk.io")
+            {
+                scheme = "https";
+            }
+            var callbackUrl = $"{scheme}{Uri.SchemeDelimiter}{HttpContext.Request.Host}";
             var encodedHost = HttpUtility.UrlEncode(callbackUrl);
 
             // prepare scope
