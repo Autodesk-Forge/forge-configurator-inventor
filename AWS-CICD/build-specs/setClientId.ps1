95a10ebb54c2a7998c31caa0deb09cfe352e771c
Michal Hradil <michal.hradil@autodesk.com>
2020-06-25 14:20:03 +0200

    differnt way

--- AWS-CICD/build-specs/setClientId.ps1
+++ AWS-CICD/build-specs/setClientId.ps1
@@ -2,9 +2,9 @@ $userIndex=$Env:CODEBUILD_BUILD_NUMBER % $Env:UserCount
 echo $userIndex
 $FORGE_CLIENT_ID=(Get-ChildItem -Path Env:\FORGE_CLIENT_ID$userIndex).Value
 echo $FORGE_CLIENT_ID
-[System.Environment]::SetEnvironmentVariable('FORGE_CLIENT_ID',"$FORGE_CLIENT_ID", [System.EnvironmentVariableTarget]::User)
+$Env:FORGE_CLIENT_ID=$FORGE_CLIENT_ID
 echo $Env:FORGE_CLIENT_ID
 $FORGE_CLIENT_SECRET=(Get-ChildItem -Path Env:\FORGE_CLIENT_SECRET$userIndex).Value
 echo $FORGE_CLIENT_SECRET
-[System.Environment]::SetEnvironmentVariable('FORGE_CLIENT_SECRET',"$FORGE_CLIENT_SECRET", [System.EnvironmentVariableTarget]::User)
+$Env:FORGE_CLIENT_SECRET=$FORGE_CLIENT_SECRET
 echo $Env:FORGE_CLIENT_SECRET
