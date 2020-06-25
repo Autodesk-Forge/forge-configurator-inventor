78325824c5df6a164e38756bec9b4dabe3edf4e1
Michal Hradil <michal.hradil@autodesk.com>
2020-06-25 14:54:00 +0200

    using the config file instead of env vars

--- AWS-CICD/build-specs/setClientId.ps1
+++ AWS-CICD/build-specs/setClientId.ps1
@@ -2,9 +2,6 @@ $userIndex=$Env:CODEBUILD_BUILD_NUMBER % $Env:UserCount
 echo $userIndex
 $FORGE_CLIENT_ID=(Get-ChildItem -Path Env:\FORGE_CLIENT_ID$userIndex).Value
 echo $FORGE_CLIENT_ID
-$Env:FORGE_CLIENT_ID=$FORGE_CLIENT_ID
-echo $Env:FORGE_CLIENT_ID
 $FORGE_CLIENT_SECRET=(Get-ChildItem -Path Env:\FORGE_CLIENT_SECRET$userIndex).Value
 echo $FORGE_CLIENT_SECRET
-$Env:FORGE_CLIENT_SECRET=$FORGE_CLIENT_SECRET
-echo $Env:FORGE_CLIENT_SECRET
+(Get-Content -path .\AWS-CICD\build-specs\appsettings.Local.json -Raw) -replace '<YOUR_CLIENT_ID>',"$FORGE_CLIENT_ID" -replace '<YOUR_CLIENT_SECRET>',"$FORGE_CLIENT_SECRET" | Set-Content -Path WebApplication\appsettings.Local.json
