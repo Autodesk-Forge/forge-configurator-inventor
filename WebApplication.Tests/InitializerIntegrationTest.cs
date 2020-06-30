df880e72e3380eb1a705d9e15384efba074304ce
Michal Hradil <michal.hradil@autodesk.com>
2020-06-30 14:23:06 +0200

    test fix

--- WebApplication.Tests/InitializerIntegrationTest.cs
+++ WebApplication.Tests/InitializerIntegrationTest.cs
@@ -99,14 +99,14 @@ namespace WebApplication.Tests
 
         public async Task InitializeAsync()
         {
-            await initializer.ClearAsync();
+            await initializer.ClearAsync(false);
         }
 
         public async Task DisposeAsync()
         {
             testFileDirectory.Delete(true);
             httpClient.Dispose();
-            await initializer.ClearAsync();
+            await initializer.ClearAsync(false);
         }
 
         private async Task<string> DownloadTestComparisonFile(string url, string name)
