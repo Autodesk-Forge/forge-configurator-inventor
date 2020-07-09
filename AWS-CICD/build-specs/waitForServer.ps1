fe13aa649d117e82284a653ca3b5a230deb03a1a
Michal Hradil <michal.hradil@autodesk.com>
2020-07-09 15:29:09 +0200

    more tests

--- AWS-CICD/build-specs/waitForServer.ps1
+++ AWS-CICD/build-specs/waitForServer.ps1
@@ -1,6 +1,7 @@
+echo "Powershell version: $PSVersionTable.PSVersion"
 while ($statusCodeInt -ne 200) {
   $response = try {
-    (Invoke-WebRequest -Uri 'https://localhost:5001' -ErrorAction Stop)
+    (Invoke-WebRequest -Uri 'https://localhost:5001' -UseBasicParsing -SslProtocol Tls12 -ErrorAction Stop)
   } catch [System.Net.WebException] {
     echo "An exception was caught: $($_.Exception.Message)"
     $_.Exception.Response
