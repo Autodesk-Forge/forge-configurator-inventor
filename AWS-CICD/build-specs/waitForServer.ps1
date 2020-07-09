422a4bbd28d4fbbe2b7936713a508b8daa525d4c
Michal Hradil <michal.hradil@autodesk.com>
2020-07-09 09:56:46 +0200

    some tests

--- AWS-CICD/build-specs/waitForServer.ps1
+++ AWS-CICD/build-specs/waitForServer.ps1
@@ -1,6 +1,12 @@
-while ($response.StatusCode -ne 200) {
-  try { 
-    $response = Invoke-WebRequest https://localhost:5001
-  } catch {
+while ($statusCodeInt -ne 200) {
+  $response = try {
+    (Invoke-WebRequest -Uri 'https://localhost:5001' -ErrorAction Stop)
+  } catch [System.Net.WebException] {
+    echo "An exception was caught: $($_.Exception.Message)"
+    $_.Exception.Response
   }
+
+  echo $response
+  $statusCodeInt = [int]$response.BaseResponse.StatusCode
+  # echo $statusCodeInt
 }
