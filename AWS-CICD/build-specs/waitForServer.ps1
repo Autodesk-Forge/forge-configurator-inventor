3c250a46775b48e8d0fd881e5c01c2c2e9bb81c8
Michal Hradil <mhradil@seznam.cz>
2020-07-15 14:24:00 +0200

    better wait for server to initialize (#210)
    This PR brings the improved wait time for the server to initialize. Before we were waiting 5 minutes. Now we wait until server returns response code 200.

---
+++ AWS-CICD/build-specs/waitForServer.ps1
@@ -0,0 +1,38 @@
+# turn off warning about self-signed certificate
+
+if (-not("dummy" -as [type])) {
+    add-type -TypeDefinition @"
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public static class Dummy {
+    public static bool ReturnTrue(object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors) { return true; }
+
+    public static RemoteCertificateValidationCallback GetDelegate() {
+        return new RemoteCertificateValidationCallback(Dummy.ReturnTrue);
+    }
+}
+"@
+}
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [dummy]::GetDelegate()
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+while ($statusCode -ne 200) {
+  # wait a bit between requests
+  sleep 10
+  # make request agains local server
+  $statusCode = try {
+    $responce = $(Invoke-WebRequest -Uri 'https://localhost:5001' -UseBasicParsing -ErrorAction Stop)
+    $responce.StatusCode
+  } catch {
+    404
+  }
+}
+
+Write-Host "Server responded. Waiting is over."
