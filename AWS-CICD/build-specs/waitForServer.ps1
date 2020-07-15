be810ba4690324ed17f3dc8655cf3cfc644096d9
Michal Hradil <michal.hradil@autodesk.com>
2020-07-15 12:32:21 +0200

    next attempt

--- AWS-CICD/build-specs/waitForServer.ps1
+++ AWS-CICD/build-specs/waitForServer.ps1
@@ -1,17 +1,36 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-while ($statusCode -ne 200) {
-  $statusCode = try {
-    (Invoke-WebRequest -Uri 'https://localhost:5001' -UseBasicParsing -ErrorAction Stop).BaseResponce.StatusCode
-  } catch [System.Net.WebException] {
-    echo "An exception was caught: $($_.Exception.Message)"
-    if ($_.Exception.Message.IndexOf("The underlying connection was closed") -eq 0) {
-      200
-    } else {
-      0
+# turn off warning about self-signed certificate
+
+if (-not("dummy" -as [type])) {
+    add-type -TypeDefinition @"
+using System;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public static class Dummy {
+    public static bool ReturnTrue(object sender,
+        X509Certificate certificate,
+        X509Chain chain,
+        SslPolicyErrors sslPolicyErrors) { return true; }
+
+    public static RemoteCertificateValidationCallback GetDelegate() {
+        return new RemoteCertificateValidationCallback(Dummy.ReturnTrue);
     }
+}
+"@
+}
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = [dummy]::GetDelegate()
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+while ($statusCode -ne " 200") {
+  $statusCode = try {
+    $responce = $(Invoke-WebRequest -Uri 'https://localhost:5001' -UseBasicParsing -ErrorAction Stop)
+    $responce.StatusCode
+  } catch {
+    404
   }
 
-  echo "StatusCode:"
-  echo $statusCode
+  Write-Host "StatusCode: --${statusCode}--"
   sleep 10
 }
