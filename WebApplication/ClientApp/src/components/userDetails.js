486f1f98d630cd6f000a53d6fa07355967ce410b
obergrd <56022635+obergrd@users.noreply.github.com>
2020-07-22 13:53:55 +0200

    used @hig/button (#235)

--- WebApplication/ClientApp/src/components/userDetails.js
+++ WebApplication/ClientApp/src/components/userDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Avatar from '@hig/avatar';
 import "./userDetails.css";
+import Button from '@hig/button';
 
 class UserDetails extends Component {
     constructor(props) {
@@ -29,11 +30,13 @@ class UserDetails extends Component {
                     <Avatar className={"avatar-custom-style"} size="large" name={this.props.profile.name} image={this.props.profile.avatarUrl} />
                 </div>
                 <span className="username">{this.props.profile.name}</span>
-                <div className="auth-button">
-                    <span className="auth-button-text" onClick={this.handleAuthClick}>
-                        {this.props.profile.isLoggedIn ? "Sign Out" : "Sign In"}
-                    </span>
-                </div>
+                <Button className="auth-button" style={
+                            { width: '244px', height: '36px', borderRadius: '2px', border: '1px solid rgb(128, 128, 128)', margin: '12px'}}
+                            type="secondary"
+                            size="small"
+                            title={this.props.profile.isLoggedIn ? "Sign Out" : "Sign In"}
+                            onClick={this.handleAuthClick}
+                        />
                 {/* Use a hidden iframe to make a logout request to autodesk since it isn't supported in a better way yet */}
                 <iframe id="hiddenLogoutFrame" />
             </div>
