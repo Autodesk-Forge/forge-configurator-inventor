b6357b9bbd9565e936a75cac7359c6ed3f44c280
Eugene Rymski <erymski@gmail.com>
2020-07-27 13:33:47 +0300

    Insert copyrighteverywhere

--- AppBundles/ExtractParametersPlugin/iLogicUtility.cs
+++ AppBundles/ExtractParametersPlugin/iLogicUtility.cs
@@ -1,6 +1,6 @@
 ﻿/////////////////////////////////////////////////////////////////////
 // Copyright (c) Autodesk, Inc. All rights reserved
-// Written by Forge Partner Development
+// Written by Forge Design Automation team for Inventor
 //
 // Permission to use, copy, modify, and distribute this software in
 // object code form for any purpose and without fee is hereby granted,
