4b45d53a1322028d90022fb851bbbc086ea8c881
Michal Hradil <michal.hradil@autodesk.com>
2020-07-30 11:14:51 +0200

    added second method of data migration

--- WebApplication/Controllers/DataMigration.cs
+++ WebApplication/Controllers/DataMigration.cs
@@ -16,14 +16,20 @@
 // UNINTERRUPTED OR ERROR FREE.
 /////////////////////////////////////////////////////////////////////
 
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using Autodesk.Forge.Model;
+using Shared;
 using WebApplication.Definitions;
 using WebApplication.State;
 using WebApplication.Processing;
 using WebApplication.Services;
+using WebApplication.Utilities;
+using WebApplication.Middleware;
 
 namespace WebApplication.Controllers
 {
@@ -35,19 +41,48 @@ namespace WebApplication.Controllers
         private readonly UserResolver _userResolver;
         private readonly ProjectWork _projectWork;
         private readonly OssBucket _bucket;
+        private readonly IForgeOSS _forgeOSS;
+        private readonly LocalCache _localCache;
 
         public DataMigrationController(ILogger<DataMigrationController> logger, IOptions<DefaultProjectsConfiguration> optionsAccessor,
-                            ProjectWork projectWork, UserResolver userResolver)
+                            ProjectWork projectWork, UserResolver userResolver, IForgeOSS forgeOSS, LocalCache localCache)
         {
             _logger = logger;
             _userResolver = userResolver;
             _bucket = _userResolver.AnonymousBucket;
             _projectWork = projectWork;
             _defaultProjectsConfiguration = optionsAccessor.Value;
+            _forgeOSS = forgeOSS;
+            _localCache = localCache;
         }
 
         [HttpGet]
         public async Task<string> Get()
+        {
+            string returnValue = "";
+            List<ObjectDetails> ossFiles = await _forgeOSS.GetBucketObjectsAsync(_bucket.BucketKey, "cache/");
+            foreach (ObjectDetails file in ossFiles)
+            {
+                string[] fileParts = file.ObjectKey.Split('/');
+                string project = fileParts[1];
+                string hash = fileParts[2];
+                string fileName = fileParts[3];
+                if (fileName == "parameters.json")
+                {
+                    returnValue += "Project " + project + " (" + hash + ") is being updated\n";
+                    string paramsFile = Path.Combine(_localCache.LocalRootName, "params.json");
+                    await _bucket.DownloadFileAsync(file.ObjectKey, paramsFile);
+                    InventorParameters inventorParameters = Json.DeserializeFile<InventorParameters>(paramsFile);
+                    await _projectWork.DoSmartUpdateAsync(inventorParameters, project, true);
+                    returnValue += "Project " + project + " (" + hash + ") was updated\n";
+                }
+            }
+
+            return returnValue;
+        }
+
+        [HttpGet("Adopt")]
+        public async Task<string> AdoptDefaultOnly()
         {
             string returnValue = "";
             foreach (DefaultProjectConfiguration defaultProjectConfig in _defaultProjectsConfiguration.Projects)
