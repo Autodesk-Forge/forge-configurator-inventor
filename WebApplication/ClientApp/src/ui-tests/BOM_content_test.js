856c33928cc2cdfbbf02e43257e924de7a0c95fb
Pavel Holecek <pavel.holecek@autodesk.com>
2020-08-05 11:04:01 +0200

    Adding new UI test for BOM content tab

---
+++ WebApplication/ClientApp/src/ui-tests/BOM_content_test.js
@@ -0,0 +1,79 @@
+/* eslint-disable jest/no-standalone-expect */
+/////////////////////////////////////////////////////////////////////
+// Copyright (c) Autodesk, Inc. All rights reserved
+// Written by Forge Design Automation team for Inventor
+//
+// Permission to use, copy, modify, and distribute this software in
+// object code form for any purpose and without fee is hereby granted,
+// provided that the above copyright notice appears in all copies and
+// that both that copyright notice and the limited warranty and
+// restricted rights notice below appear in all supporting
+// documentation.
+//
+// AUTODESK PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS.
+// AUTODESK SPECIFICALLY DISCLAIMS ANY IMPLIED WARRANTY OF
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR USE.  AUTODESK, INC.
+// DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
+// UNINTERRUPTED OR ERROR FREE.
+/////////////////////////////////////////////////////////////////////
+
+/* eslint-disable no-undef */
+const locators = require('./elements_definition.js');
+const assert = require('assert');
+//const { debug } = require('console');
+
+const bomTable = '//div[@class="BaseTable__body"]';
+//const parametersElement = '.parameters';
+const bomRows = '//div[@class="BaseTable__row"]';
+
+let csvRow = new Array();
+csvRow.push('IAM/IPT,Model'); //data from CSV
+csvRow.push('RFA,Model');
+
+Before((I) => {
+    I.amOnPage('/');
+});
+
+Feature('Bom Data Validation');
+
+// validate that Parameter notification is displayed
+Scenario('should check BOM data after change', async (I) => {
+
+    // select project in the Project Switcher
+    I.selectProject('Conveyor');
+
+    // click to BOM tab
+    I.click(locators.downloadsTab); //temporary use downloads
+    I.waitForVisible(bomTable, 5);
+
+    const data = await I.grabTextFrom(bomRows);
+    I.say('***********************************');
+    I.say(csvRow);
+
+    for(let i=0; i< data.length; ++i)
+    {
+        const htmlRow = data[i].replace('\n', ',');
+        assert.equal(csvRow[i], htmlRow, 'Error: BOM row is not identical with CSV!');
+    }
+
+    /*
+    // change paramter
+    I.waitForElement(parametersElement, 20);
+    I.setParamValue("LEGS (MIN:12 | MAX:8 | STEP:2)", "6");
+
+    // Click on Update button
+    I.waitForVisible(locators.xpButtonUpdate, 10);
+    I.click(locators.xpButtonUpdate);
+
+    // wait for progress bar shows and disappears
+    I.waitForVisible(updatingProjectProgress, 10);
+    I.waitForInvisible(updatingProjectProgress, 120);
+    */
+
+   for(let i=0; i< data.length; ++i)
+   {
+       const htmlRow = data[i].replace('\n', ',');
+       assert.equal(csvRow[i], htmlRow, 'Error: BOM row is not identical with CSV!');
+   }
+
+  });
