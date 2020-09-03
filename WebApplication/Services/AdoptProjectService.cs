160c8f4d8b72b6c09c13154b11ad65870004299d
Miroslav Šimko <miroslav.simko@autodesk.com>
2020-09-03 13:40:09 +0200

    Add skeleton of AdoptProjectService

---
+++ WebApplication/Services/AdoptProjectService.cs
@@ -0,0 +1,18 @@
+﻿using Shared;
+using WebApplication.Definitions;
+
+namespace WebApplication.Services
+{
+    public class AdoptProjectService
+    {
+        /// <summary>
+        /// https://jira.autodesk.com/browse/INVGEN-45256
+        /// </summary>
+        /// <param name="projectConfiguration"></param>
+        /// <param name="parameters"></param>
+        public void AdoptProjectWithParameters(DefaultProjectConfiguration projectConfiguration, InventorParameters parameters)
+        {
+
+        }
+    }
+}
