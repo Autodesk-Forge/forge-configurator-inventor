6d42b5ebea4bccb74b03ae839d85be49f8d8af0b
Miroslav Šimko <miroslav.simko@autodesk.com>
2020-09-04 09:12:59 +0200

    Added skeleton for integration test of project adoption service

---
+++ WebApplication.Tests/Integration/AdoptProjectServiceTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Shared;
+using WebApplication.Definitions;
+using WebApplication.Services;
+using Xunit;
+
+namespace WebApplication.Tests.Integration
+{
+    public class AdoptProjectServiceTest : IClassFixture<WebApplicationFactory<WebApplication.Startup>>
+    {
+        private readonly WebApplicationFactory<WebApplication.Startup> _factory;
+
+        public AdoptProjectServiceTest(WebApplicationFactory<WebApplication.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        public class AdoptProjectWithParametersDataProvider : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new AdoptProjectWithParametersPayload()
+                    {
+                        Name = "TestWrench",
+                        Url = "https://sdra-default-projects.s3-us-west-2.amazonaws.com/Wrench_2021.zip",
+                        TopLevelAssembly = "Wrench.iam",
+                        Config = new InventorParameters()
+                        {
+                            {
+                                "Some Param", new InventorParameter()
+                                {
+                                    Label = "Some Label"
+                                }
+                            }
+                        }
+                    }
+                };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        [Theory]
+        [ClassData(typeof(AdoptProjectWithParametersDataProvider))]
+        public void AdoptProjectWithParameters(AdoptProjectWithParametersPayload payload)
+        {
+            var service = (AdoptProjectService) _factory.Services.GetService(typeof(AdoptProjectService));
+
+            service.AdoptProjectWithParameters(payload);
+        }
+    }
+}
