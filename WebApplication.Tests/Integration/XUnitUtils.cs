f021e581cfa39fdd68c6fa11c6e204296fb94951
Miroslav Šimko <miroslav.simko@autodesk.com>
2020-09-16 10:30:43 +0200

    unrevert commit

---
+++ WebApplication.Tests/Integration/XUnitUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Xunit.Abstractions;
+
+namespace WebApplication.Tests.Integration
+{
+    internal static class XUnitUtils
+    {
+        public static void RedirectConsoleToXUnitOutput(ITestOutputHelper output)
+        {
+            Console.SetOut(new XUnitLogWriter(output));
+        }
+        private class XUnitLogWriter : StringWriter
+        {
+            private readonly ITestOutputHelper _output;
+
+            public XUnitLogWriter(ITestOutputHelper output)
+            {
+                _output = output;
+            }
+
+            public override void Write(string? value)
+            {
+                _output.WriteLine(value?.TrimEnd());
+            }
+        }
+    }
+}
