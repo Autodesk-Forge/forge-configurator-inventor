a1e31d600a80925f391f1b92d4d683da78d447a4
Miroslav Šimko <miroslav.simko@autodesk.com>
2020-10-07 12:29:29 +0200

    INVGEN-45782: Stop polling server to get progress INVGEN-46493: Implement the URL builder for callbacks
    Add UrlBuilder singleton bean with configurable URLs

---
+++ WebApplication/Services/UrlBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Options;
+using WebApplication.Definitions;
+
+namespace WebApplication.Services
+{
+    public class UrlBuilder
+    {
+        private readonly CallbackUrls _callbackUrlsConfiguration;
+
+        public UrlBuilder(IOptions<CallbackUrls> callbackUrlsConfiguration)
+        {
+            _callbackUrlsConfiguration = callbackUrlsConfiguration.Value;
+        }
+
+        public string GetUpdateCallbackUrl(string clientId, string hash, string projectId, string prefix, string jobId)
+        {
+            return string.Format(_callbackUrlsConfiguration.Methods.Update, _callbackUrlsConfiguration.Base, 
+                clientId, hash, projectId, prefix, jobId);
+        }
+    }
+}
