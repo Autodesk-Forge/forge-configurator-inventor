89e43124b11fa14d31848a57675f85ec0fb08d58
Miroslav Šimko <miroslav.simko@autodesk.com>
2020-10-16 13:10:55 +0200

    test setup simplification

--- WebApplication.Tests/PublisherTest.cs
+++ WebApplication.Tests/PublisherTest.cs
@@ -18,81 +18,81 @@ namespace WebApplication.Tests
 {
     public class PublisherTest
     {
+        private readonly Dictionary<string, IArgument> _workItemArgs;
+        private readonly Mock<IForgeAppBase> _configMock;
+
+        public PublisherTest()
+        {
+            _workItemArgs = new Dictionary<string, IArgument>();
+
+            _configMock = new Mock<IForgeAppBase>();
+            _configMock.Setup(mock => mock.ActivityId).Returns("activityId");
+            _configMock.Setup(mock => mock.ActivityLabel).Returns("activityLabel");
+        }
+
         private class CallbackDataProvider : IEnumerable<object[]>
         {
             private readonly Publisher _publisher;
 
-            private readonly Dictionary<string, IArgument> _workItemArgs;
-            private readonly Mock<IForgeAppBase> _configMock;
             private readonly Mock<IWorkItemsApi> _workItemsApiMock;
 
-            private readonly string _trackingKey = Guid.NewGuid().ToString("N");
-
-            private readonly WorkItemStatus _workItemStatus = new WorkItemStatus
-            {
-                Status = Status.FailedLimitDataSize
-            };
+            private const string _trackingKey = "cd26ccf675d64521884f1693c62ed303";
 
             public CallbackDataProvider()
             {
-                var logger = new SerilogLoggerFactory().CreateLogger<Publisher>();
-                var resourceProviderMock = new Mock<IResourceProvider>();
-                var postProcessingMock = new Mock<IPostProcessing>();
-                var publisherConfiguration = new PublisherConfiguration
-                {
-                    CallbackUrlBase = "http://fci/complete/",
-                    CompletionCheck = CompletionCheck.Callback
-                };
-                var publisherConfigurationOptions = Options.Create(publisherConfiguration);
+                const string callbackUrlBase = "http://fci/complete/";
 
-                var workItem = new WorkItem
-                {
-                    ActivityId = "nickname.activityId+activityLabel",
-                    Arguments = _workItemArgs
-                };
+                var resourceProviderMock = new Mock<IResourceProvider>();
+                resourceProviderMock.SetupGet(mock => mock.Nickname).Returns(Task.FromResult("nickname"));
+
+                var guidGenerator = new Mock<IGuidGenerator>();
+                guidGenerator.Setup(mock => mock.GenerateGuid()).Returns(_trackingKey);
 
                 _workItemsApiMock = new Mock<IWorkItemsApi>();
-
                 _workItemsApiMock.Setup(mock => mock.CreateWorkItemAsync(
-                        It.Is<WorkItem>(wi => wi.ActivityId.Equals(workItem.ActivityId)
+                        It.Is<WorkItem>(wi => wi.ActivityId.Equals("nickname.activityId+activityLabel")
                                               && ((XrefTreeArgument) wi.Arguments["onComplete"]).Url.StartsWith(
-                                                  publisherConfiguration.CallbackUrlBase)),
+                                                  callbackUrlBase)),
                         null, null, true))
                     .Returns(Task.FromResult(new ApiResponse<WorkItemStatus>(null, new WorkItemStatus
                     {
                         Status = Status.Success
                     })));
 
-                var guidGenerator = new Mock<IGuidGenerator>();
-                guidGenerator.Setup(mock => mock.GenerateGuid()).Returns(_trackingKey);
-
                 _publisher = new Publisher(
                     null,
-                    logger,
+                    new SerilogLoggerFactory().CreateLogger<Publisher>(),
                     resourceProviderMock.Object,
-                    postProcessingMock.Object,
-                    publisherConfigurationOptions,
+                    new Mock<IPostProcessing>().Object,
+                    Options.Create(new PublisherConfiguration
+                    {
+                        CallbackUrlBase = callbackUrlBase,
+                        CompletionCheck = CompletionCheck.Callback
+                    }),
                     _workItemsApiMock.Object,
                     guidGenerator.Object);
-
-                resourceProviderMock.SetupGet(mock => mock.Nickname).Returns(Task.FromResult("nickname"));
-
-                _configMock = new Mock<IForgeAppBase>();
-                _configMock.Setup(mock => mock.ActivityId).Returns("activityId");
-                _configMock.Setup(mock => mock.ActivityLabel).Returns("activityLabel");
-
-                _workItemArgs = new Dictionary<string, IArgument>();
             }
 
             public IEnumerator<object[]> GetEnumerator()
             {
                 yield return new object[]
                 {
-                    _publisher, 
-                    _workItemArgs, 
-                    _configMock.Object, 
-                    _trackingKey, 
-                    _workItemStatus, 
+                    _publisher,
+                    _trackingKey,
+                    new WorkItemStatus
+                    {
+                        Status = Status.FailedLimitDataSize
+                    },
+                    _workItemsApiMock
+                };
+                yield return new object[]
+                {
+                    _publisher,
+                    _trackingKey,
+                    new WorkItemStatus
+                    {
+                        Status = Status.Cancelled
+                    },
                     _workItemsApiMock
                 };
             }
@@ -102,17 +102,19 @@ namespace WebApplication.Tests
 
         [Theory]
         [ClassData(typeof(CallbackDataProvider))]
-        public void RunWorkItemAsyncUsingCallback(Publisher publisher, Dictionary<string, IArgument> workItemArgs, 
-            IForgeAppBase config, string trackingKey, WorkItemStatus workItemStatus, 
+        public void RunWorkItemAsyncUsingCallback(Publisher publisher,
+            string trackingKey, WorkItemStatus workItemStatus,
             Mock<IWorkItemsApi> workItemsApiMock)
         {
-            var workItemTask = publisher.RunWorkItemAsync(workItemArgs, config);
+            var workItemTask = publisher.RunWorkItemAsync(_workItemArgs, _configMock.Object);
             publisher.NotifyTaskIsCompleted(trackingKey, workItemStatus);
             workItemTask.Wait();
-            
+
             workItemsApiMock.Verify(mock => mock.CreateWorkItemAsync(It.IsAny<WorkItem>(), null, null, true),
                 Times.Once);
             Assert.Equal(workItemStatus.Status, workItemTask.Result.Status);
+            
+            workItemsApiMock.Invocations.Clear();
         }
     }
 }
