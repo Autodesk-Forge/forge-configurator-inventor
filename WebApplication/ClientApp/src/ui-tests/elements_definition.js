6a700a6c249eecb0c3ebb4522713a46cb9ebec9c
Zdenek Slavik <zdenek.slavik@autodesk.com>
2021-01-04 12:19:52 +0100

    fix one more locator to be exact

--- WebApplication/ClientApp/src/ui-tests/elements_definition.js
+++ WebApplication/ClientApp/src/ui-tests/elements_definition.js
@@ -50,7 +50,9 @@ module.exports = {
      xpFirstInputOnModelTab : '//*[@id="model"]/div/div[1]/div[2]/div[1] //input',
      FDAActionTimeout: 600,
      xpButtonExportPDF: locate('button').find('span').withText('Export PDF'),
-     getProjectFromSwitcher: (name) => locate('li').find('span').withAttr({role: 'button'}).withText(name),
+     getProjectFromSwitcher: (name) => { xp = '//li[contains(@role,"menuitem") and .//span[text()="' + name + '"]]';
+          return locate(xp);
+     },
      getProjectByName: (name) => locate('div').withAttr({role: 'gridcell'}).find('div').withText(name),
      getProjectRowByName: (name) => locate('div').withAttr({role: 'row'}).withChild('div').withText(name),
      xpErrorMessageTitle: '//div[@class="modalFailContent"]//p[contains(@class,"errorMessageTitle")]',
