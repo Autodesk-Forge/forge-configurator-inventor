bcde4da7cf641612054b190c2ed85aed7b0cf90b
ake133c <97872487+ake133c@users.noreply.github.com>
2022-06-23 21:59:23 +0200

    Fixing lint errors and warnings

--- WebApplication/ClientApp/src/setupProxy.js
+++ WebApplication/ClientApp/src/setupProxy.js
@@ -1,15 +1,16 @@
-﻿const proxy = require('http-proxy-middleware');
+﻿/* eslint-disable no-undef */
+const proxy = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
     app.use(proxy('/signalr',
-        { target: 'https://localhost:5001/', "changeOrigin": false, "secure": false, ws: true}
+        { target: 'https://localhost:5001/', "changeOrigin": false, "secure": false, ws: true }
     ));
 
     app.use(proxy('/login',
-        { target: 'https://localhost:5001/', "changeOrigin": true, "secure": false}
+        { target: 'https://localhost:5001/', "changeOrigin": true, "secure": false }
     ));
 
     app.use(proxy('/ClearSelf',
         { target: 'https://localhost:5001/', "changeOrigin": true, "secure": false }
     ));
-}
+};
